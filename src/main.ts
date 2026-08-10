dc10c359f8556705226bff073396887451d2cf5f
zeo <zeo@grepp.co>
2026-08-10 10:37:51 +0900

    fix: build error [PGS-1863]

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,4 @@ import { worker } from "./mocks";
 import "./monaco-worker";
 import { router } from "./router/index.ts";
 
-await worker.start();
-
-createApp(App).use(router).mount("#app");
+worker.start().then(() => createApp(App).use(router).mount("#app"));
